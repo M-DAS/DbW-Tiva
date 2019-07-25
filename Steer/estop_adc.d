73c58f5abb2f8efa9074e584ddef38644bd42da9
athanasios <aargyris@umich.edu>
2019-07-24 22:04:10 -0400

    plz

--- Steer/estop_adc.d
+++
@@ -1,9 +0,0 @@
-.\estop_adc.o: Estop_ADC.c
-.\estop_adc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\estop_adc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
-.\estop_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\inc/tm4c123gh6pm.h
-.\estop_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\inc/hw_memmap.h
-.\estop_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\driverlib/sysctl.h
-.\estop_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\driverlib/gpio.h
-.\estop_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\driverlib/adc.h
-.\estop_adc.o: Estop_ADC.h
--- brakeCL/pressure_adc.d
+++
@@ -1,9 +0,0 @@
-.\pressure_adc.o: Pressure_ADC.c
-.\pressure_adc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\pressure_adc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
-.\pressure_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\inc/tm4c123gh6pm.h
-.\pressure_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\inc/hw_memmap.h
-.\pressure_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\driverlib/sysctl.h
-.\pressure_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\driverlib/gpio.h
-.\pressure_adc.o: C:\ti\TivaWare_C_Series-2.1.4.178\driverlib/adc.h
-.\pressure_adc.o: Globals_and_Defines.h
